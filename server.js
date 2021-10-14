c8f2e77be148ae457a236a7af7e3b8c394642057
Rick Martin <rmartin1985@att.net>
2021-10-14 16:25:00 -0500

    fetch calls for animals and index

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const { animals } = require('./data/animals.json');
-const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals.json');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 const { type } = require('os');
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -113,6 +116,22 @@ app.post('/api/animals', (req, res) => {
 
         res.json(req.body);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
